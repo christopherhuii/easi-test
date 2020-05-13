8822b4e47845be1871f57fa24e0399acf5b26631
Christopher Hui <christopher.kam.hui@gmail.com>
2020-05-12 22:51:34 -0700

    initial commit for login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, useHistory } from 'react-router-dom';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+import OktaSignIn from '@okta/okta-signin-widget/dist/js/okta-sign-in.min';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <AuthWrapper>
+          <Route path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <SecureRoute path="/protected" component={Protected} />
+          <Route path="/implicit/callback" component={LoginCallback} />
+        </AuthWrapper>
+      </BrowserRouter>
     </div>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+);
+
+const Login = () => (
+  <div>
+    <h1>Login</h1>
+    <OktaSignInWidget />
+  </div>
+);
+
+class OktaSignInWidget extends Component {
+  componentDidMount() {
+    this.widget = new OktaSignIn({
+      baseUrl: 'https://test.idp.idm.cms.gov',
+      authParams: {
+        pkce: true,
+        responseMode: 'query',
+      },
+      el: '#sign-in-widget',
+    });
+
+    this.widget.showSignInToGetTokens({
+      authorizationServerId: 'aus2e96etlbFPnBHt297',
+      clientId: '0oa2e913coDQeG19S297',
+      redirectUri: 'http://localhost:3000/implicit/callback',
+      scope: 'openid profile email',
+    });
+  }
+
+  componentWillUnmount() {
+    this.widget.remove();
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="sign-in-widget" />
+      </div>
+    );
+  }
+}
+
+const Protected = () => (
+  <div>
+    <h1>Protected</h1>
+  </div>
+);
+
+const AuthWrapper = ({ children }) => {
+  const history = useHistory();
+
+  const handleAuthRequiredRedirect = () => {
+    history.push('/login');
+  };
+  return (
+    <Security
+      issuer="https://test.idp.idm.cms.gov/oauth2/aus2e96etlbFPnBHt297"
+      clientId="0oa2e913coDQeG19S297"
+      redirectUri="http://localhost:3000/implicit/callback"
+      onAuthRequired={handleAuthRequiredRedirect}
+      responseType={['code']}
+      pkce
+    >
+      {children}
+    </Security>
+  );
+};
 
 export default App;
